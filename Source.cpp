04c991cad63c2c52280c875ac4a276ed3023e192
M.Morawski <moras.technika@gmail.com>
2018-01-19 23:19:37 +0100

    Dodano oblicanie delty w programie glownym

--- Source.cpp
+++ Source.cpp
@@ -8,7 +8,14 @@
 
 using namespace std;
 
+//deklaracja zmiennych
+double a, b, c, delta, x1, x2;
+double eps=0.000000001;
+
 int main(){
+
+//pobieramy dane od uzytkownika
+
 	cout << "Podaj wspolczynniki rownania w postaci axx + bx + c = 0" << endl;
 	cout << "Podaj a = ";
 	cin >> a;
@@ -16,6 +23,17 @@ int main(){
 	cin >> b;
 	cout << "Podaj c = ";
 	cin >> c;
+
+//obliczamy delte
+
+delta(a, b, c, delta);
+
+//sprawdzamy blad obliczen zmiennoprzecinkowych
+
+if (abs(delta) < eps) {
+			delta = 0.0;
+		}
+
 
 
 	_getche();
